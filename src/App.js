118b2818d6f1ee2ed1eec104f72277b5d51c52f4
Kevin Turney <super996@optonline.net>
2018-10-25 13:29:29 -0400

    convert to scss, add title

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <header className="title elegantShadow">Discography</header>
       </div>
     );
   }
