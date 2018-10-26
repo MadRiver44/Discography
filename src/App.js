2ff77d352939a9ce6b071c2e6cfcf102e8539f28
Kevin Turney <super996@optonline.net>
2018-10-26 11:21:24 -0400

    import Search, Albums

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Search from './Search.js';
+import Albums from './Albums.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="title elegantShadow">Discography</header>
+        <Search />
+        <Albums />
       </div>
     );
   }
