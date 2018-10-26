5581d29f9432f9780847f9753c8b4293e4840ed7
Kevin Turney <super996@optonline.net>
2018-10-26 11:25:28 -0400

    mock up search, check functionality

---
+++ src/Search.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Button from './Button.js';
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    console.log('button is clicked');
+  }
+
+  render() {
+    return (
+      <div className="search-container">
+        <form className="form-container">
+          <div className="text-button">
+            <label htmlFor="searchbar" />
+            <input type="search" id="searchbar" />
+          </div>
+        <Button handleClick={this.handleClick} />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;
