888c21b15b56d5b03f257a2c43422ff62564884d
Kevin Turney <super996@optonline.net>
2018-10-27 13:22:13 -0400

    render list items

--- src/Albums.js
+++ src/Albums.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const Album = props => {
-  const albums = '';
-  return <div id="album-container">{albums}</div>;
+// statelesss functional component to render album container
+const Albums = props => {
+  const { items } = props;
+  console.log(props, items);
+  const listItems = items.map((item, index) => <li key={index}>{item.collectionName}</li>);
+  return <div id="album-container">{listItems}</div>;
 };
 
-export default Album;
+export default Albums;
