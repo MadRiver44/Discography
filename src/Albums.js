234a7d935a55c8542d716b4a33c79f7b5117e34d
Kevin Turney <super996@optonline.net>
2018-10-27 14:23:06 -0400

    get artwork rendered

--- src/Albums.js
+++ src/Albums.js
@@ -3,8 +3,14 @@ import React from 'react';
 // statelesss functional component to render album container
 const Albums = props => {
   const { items } = props;
-  console.log(props, items);
-  const listItems = items.map((item, index) => <li key={index}>{item.collectionName}</li>);
+  const listItems = items.map((item, index) => {
+    return (
+      <ul>
+        <li key={index}>{item.collectionName}</li>
+        <img src={item.artworkUrl100} />
+      </ul>
+    );
+  });
   return <div id="album-container">{listItems}</div>;
 };
 
