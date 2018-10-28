0c92df0cb9e1ea18ae553104c190c66b67a3cdf1
Kevin Turney <super996@optonline.net>
2018-10-28 13:55:04 -0400

    modify image size, regex to format date

--- src/Albums.js
+++ src/Albums.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
-// statelesss functional component to render album container
+// statelesss functional component to render album item-container
 const Albums = props => {
   const { items } = props;
-  const listItems = items.map((item, index) => {
-    return (
-      <ul>
-        <li key={index}>{item.collectionName}</li>
-        <img src={item.artworkUrl100} />
-      </ul>
-    );
-  });
+  const listItems = items.map((item, index) => (
+    <ul className="item-container">
+      <img
+        key={item.collectionName + index.toString()}
+        srcSet={item.artworkUrl100.replace('100x100', '300x300')}
+        alt={item.collectionName}
+      />
+      <li key={item.artistViewUrl + index.toString()}>{item.collectionName}</li>
+      <li key={item.releaseDate + index.toString()}>
+        Release Date:
+        {item.releaseDate.split(/T\d+:\d+:\d+Z/)}
+      </li>
+    </ul>
+  ));
   return <div id="album-container">{listItems}</div>;
 };
 
 export default Albums;
+
